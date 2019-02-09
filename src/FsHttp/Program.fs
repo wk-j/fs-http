3227b09334d17e64b9924cb3de4f31503b65dcdd
wk-j <somnuk.wk@gmail.com>
2019-02-10 04:04:09 +0700

    Initial commit

---
+++ src/FsHttp/Program.fs
@@ -0,0 +1,18 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open FsHttp
+
+[<EntryPoint>]
+let main argv =
+    http {
+        POST "https://regres.in/api/users"
+        CacheControl "no-cache"
+
+        body
+        json """
+            "name": "morpheus",
+            "job": "leader"
+        """
+    } |> ignore
+    0 // return an integer exit code
